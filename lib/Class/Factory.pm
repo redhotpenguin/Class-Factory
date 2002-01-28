a7930db3b2395252f9d8aec0a12255024692bf8a
Chris Winters <chris@cwinters.com>
2002-01-28 17:20:04 +0000

    fix dumb naming issue

--- lib/Class/Factory.pm
+++ lib/Class/Factory.pm
@@ -30,10 +30,13 @@ sub add_factory_type {
         die "Cannot add factory type [$factory_type] to [$class]: no class defined\n";
     }
 
-    my $factory_class = $item->get_factory_type( $factory_type );
-    if ( $factory_class ) {
+    my $map = $item->get_factory_map;
+    my $set_factory_class = ( ref $map eq 'HASH' )
+                              ? $map->{ $factory_type }
+                              : $item->get_factory_type( $factory_type );
+    if ( $set_factory_class ) {
         warn "Attempt to add type [$factory_type] to [$class] redundant; ",
-             "type already exists with class [$class]\n";
+             "type already exists with class [$set_factory_class]\n";
         return;
     }
 
@@ -117,8 +120,11 @@ Class::Factory - Base class for factory classes
 =head1 DESCRIPTION
 
 This is a simple module that factory classes can use to generate new
-types of objects on the fly. The base class defines two methods for
-subclasses to use: C<get_factory_class()> and
+types of objects on the fly, providing a consistent interface to
+common groups of objects.
+
+Implementation for subclasses is very simple. The base class defines
+two methods for subclasses to use: C<get_factory_class()> and
 C<add_factory_type()>. Subclasses must define either
 C<get_factory_map()> or both C<get_factory_type()> and
 C<set_factory_type()>.
@@ -128,6 +134,86 @@ C<set_factory_type()>.
 B<get_factory_class( $factory_type )>
 
 B<add_factory_type( $factory_type, $factory_class )>
+
+=head1 SUBCLASSING
+
+You can do this either the simple way or the flexible way. For most
+cases, the simple way will suffice:
+
+ package My::Factory;
+
+ use base qw( Class::Factory );
+
+ my %TYPES = ();
+ sub get_factory_map { return \%TYPES }
+
+If you elect to use the flexible way, you need to implement two
+methods:
+
+ package My::Factory;
+
+ use base qw( Class::Factory );
+
+ my %TYPES = ();
+ sub get_factory_class {
+     my ( $class, $type ) = @_;
+     return $TYPES{ $type };
+ }
+
+ sub set_factory_class {
+     my ( $class, $type, $factory_class ) = @_;
+     return $TYPES{ $type } = $factory_class;
+ }
+
+How these methods work is entirely up to you -- maybe
+C<get_factory_class()> does a lookup in some external resource before
+returning the class. Whatever floats your boat.
+
+=head1 USAGE
+
+=head2 Common Pattern
+
+This is a very common pattern. Subclasses create an C<initialize()>
+method that gets called when the object is created:
+
+ package My::Factory;
+
+ use base qw( Class::Factory );
+
+ my %TYPES = ();
+ sub get_factory_map { return \%TYPES }
+
+ sub new {
+     my ( $class, $type, @params ) = @_;
+     my $factory_class = $class->get_factory_class( $type );
+     my $self = bless( {}, $factory_class );
+     return $self->initialize( @params );
+ }
+
+ 1;
+
+And here is what a subclass might look like:
+
+ package My::Subclass;
+
+ use base qw( Class::Accessor );
+ my @FIELDS = qw( filename status );
+ My::Subclass->mk_accessors( @FIELDS );
+
+ # Note: we've taken the flattened C<@params> passed in and assigned
+ # the first element as C<$filename> and the other element as a
+ # hashref C<$params>
+
+ sub initialize {
+     my ( $self, $filename, $params ) = @_;
+     unless ( -f $filename ) {
+         die "Filename [$filename] does not exist. Object cannot be created";
+     }
+     foreach my $field ( @FIELDS ) {
+         $self->{ $field } = $params->{ $field } if ( $params->{ $field } );
+     }
+     return $self;
+ }
 
 =head1 COPYRIGHT
 
