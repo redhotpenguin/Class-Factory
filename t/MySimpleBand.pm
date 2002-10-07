1d43076fc95a6620d9bbc9e20c273589162a81b1
cwinters <cwinters@f8626016-021a-0410-906f-c1fffa0af479>
2002-10-07 05:08:04 +0000

    latest changes (few of them :-)
    git-svn-id: http://www.redhotpenguin.com/svn/class_factory/trunk@32 f8626016-021a-0410-906f-c1fffa0af479

--- t/MySimpleBand.pm
+++ t/MySimpleBand.pm
@@ -5,14 +5,6 @@ package MySimpleBand;
 use strict;
 use base qw( Class::Factory );
 
-#use Class::Factory;
-#@MySimpleBand::ISA = qw( Class::Factory );
-
-my %TYPES    = ();
-my %REGISTER = ();
-sub get_factory_map  { return \%TYPES }
-sub get_register_map { return \%REGISTER }
-
 sub init {
     my ( $self, $params ) = @_;
     $self->band_name( $params->{band_name} );
@@ -32,8 +24,8 @@ sub genre {
     return $self->{genre};
 }
 
-MySimpleBand->add_factory_type( rock    => 'MyRockBand' );
-MySimpleBand->register_factory_type( country => 'MyCountryBand' );
+__PACKAGE__->add_factory_type( rock => 'MyRockBand' );
+__PACKAGE__->register_factory_type( country => 'MyCountryBand' );
 
 1;
 
